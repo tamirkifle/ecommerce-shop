1b50a48197a6244e8ad9832fbca60bd4aac822ff
Tamir K. Yirga <tkifle@gmail.com>
2022-04-22 19:37:04 +0300

    fixed same AttributeSetIds causing wrong attributes

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -3,7 +3,13 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const GRAPHQL_URL = "http://localhost:4000/";
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      AttributeSet: {
+        keyFields: false,
+      },
+    },
+  }),
   uri: GRAPHQL_URL,
 });
 
