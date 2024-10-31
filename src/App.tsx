5b5a5ce3806670eb7cba6bdd9a2746e4074d9d44
Tamir K. Yirga <41894752+tamirkifle@users.noreply.github.com>
2024-10-31 07:55:34 +0300

    Added missing Navigate from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar";
 import { ProductListing, ProductDescription, Cart } from "./pages";
 import { Global } from "@emotion/react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 import styled from "@emotion/styled";
 import GlobalStyles from "./GlobalStyles";
